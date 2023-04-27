1c81aaf1f0888f3f24f97403b510445f03fa7c8c
= <=>
2023-04-26 22:23:06 -0400

    primera funcion flecha

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
+const drawn = (a, b) => {
 
-let container = document.getElementById("grid")
-container.innerHTML = ""
+    let container = document.getElementById("grid")
+    container.innerHTML = ""
 
-for (let i = 1; i <= 10; i++) {
-    let elemento = `<div class="col-6 col-md-4 mt-3">
-                    <div class="card">
-                        <h5 class="card-title">Tabla del ${i}</h5>
-                        <div class="card-body">
-                `
+    for (let i = 1; i <= a; i++) {
+        let elemento = `<div class="col-6 col-md-4 mt-3">
+                        <div class="card">
+                            <h5 class="card-title">Tabla del ${i}</h5>
+                            <div class="card-body">`
+        
+        for (let j = 1; j <= b; j++) {
+            console.log(i + " * " + j, i * j);
+            elemento += `<p class="card-text">${i} * ${j} = ${i * j}</p>`
+        }
+        elemento +=  `</div>
+                      </div>
+                      </div>`
     
-    for (let j = 1; j <= 10; j++) {
-        console.log(i + " * " + j, i * j);
-        elemento += `<p class="card-text">${i} * ${j} = ${i * j}</p>`
-    }
-    elemento +=  `</div>
-                  </div>
-                  </div>
-                `
-
-    container.innerHTML += elemento
+        container.innerHTML += elemento
+    }    
 }
+
+drawn(10,12);
